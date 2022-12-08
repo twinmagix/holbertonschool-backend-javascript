88c3032a9c5ded395f83fba1703c2b9b05a4347c
twinmagix <sazindwandwe1997@gmail.com>
2022-12-08 08:43:56 +0200

    es commit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++
@@ -1,7 +0,0 @@
-export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  return [...set]
-    .filter((el) => el.startsWith(startString))
-    .map((el) => el.slice(startString.length))
-    .join('-');
-}
